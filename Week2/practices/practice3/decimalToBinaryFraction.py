b3c78cffc6a1ce15cd484a32ea57286ccdc07b70
Syen Park <syenpark@gmail.com>
2018-06-13 21:25:22 +0900

    amend

--- Week2/practices/practice3/decimalToBinaryFraction.py
+++ Week2/practices/practice3/decimalToBinaryFraction.py
@@ -9,7 +9,8 @@ Python Version: 3.6
 x = float(input('Enter a decimal number between 0 and 1: '))
 
 p = 0
-s = ''
+digits = []
+fraction = ''
 
 while x*(2**p) % 1:
     print('Remainder = ' + str(x*(2**p) - int(x*(2**p))))
@@ -18,7 +19,7 @@ while x*(2**p) % 1:
 num = int(x*(2**p))
 
 for i in range(p):
-    s = str(num % 2) + s
+    digits.insert(0, str(num % 2))
     num //= 2
 
-print('The binary representation of the decimal', x, 'is', '0.' + s)
+print('The binary representation of the decimal', x, 'is', '0.' + fraction.join(digits))
