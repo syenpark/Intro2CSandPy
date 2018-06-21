252a1e238e5032da9fbe4ff0798be86612806b09
Syen Park <syenpark@gmail.com>
2018-06-22 00:28:20 +0900

    add pro set4-pro2

--- week4/problem_set4/ps4a.py
+++ week4/problem_set4/ps4a.py
@@ -152,7 +152,11 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
+    copy = hand.copy()
+    for c in word:
+            copy[c] -= 1
+    
+    return copy
 
 
 
