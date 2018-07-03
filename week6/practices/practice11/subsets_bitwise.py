bea8c78b8a8601d2a4ef2192205e81f20f6caa26
Syen Park <syenpark@gmail.com>
2018-07-03 20:12:37 +0900

    improve

--- week6/practices/practice11/subsets_bitwise.py
+++ week6/practices/practice11/subsets_bitwise.py
@@ -6,17 +6,23 @@ Spyder Editor
 Python Version: 3.6
 """
 def gen_subsets(items):
+    """
+    returns: all subsets
+    """
     subsets = []
     n = len(items)
     
+    # 1 << n is equal to pow(2, n)
     for i in range(1 << n):
-        temp = []
+        subset = []
         
+        # i >> j is equal to i // pow(2, j)
+        # A & 1 is equal to A % 2
         for j in range(n):
-            if (i >> j)%2:
-                temp.append(items[j])
+            if (i >> j) & 1:
+                subset.append(items[j])
                 
-        subsets.append(temp)
+        subsets.append(subset)
         
     return subsets
 
